74d9bfbd4230a43e669258be444d707d71911a2d
Yonatan Melka <ybmelka@gmail.com>
2021-01-19 19:47:59 -0600

    Add book cover image

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
+const libgen = require('libgen');
 require('dotenv').config();
+
 const token = process.env.TELEGRAM_ACCESS_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
-const libgen = require('libgen');
 let keyboard;
 // /Start
 bot.onText(/\/start/, async (msg) => {
@@ -13,7 +14,7 @@ bot.onText(/\/start/, async (msg) => {
       caption: `Welcome ${msg.from.first_name}! Find and Download Any Book By Just Sending The Title.`,
     })
     .then(() => bot.sendMessage(chatId, 'First, I will have a test run. Sit Tight!'))
-    .then(() => bookOptions(chatId, 'Magic of thinking big', 'start'))
+    .then(() => bookOptions(chatId, 'Magic of thinking big', true))
     .then(() =>
       bot.sendMessage(
         428992867,
@@ -39,28 +40,28 @@ const bookOptions = async (chatId, book, start) => {
   bot.sendMessage(chatId, 'Loading Please Wait...');
   try {
     const keyboard = await fetchData(book);
-    let message;
-    if (keyboard.length && start !== 'start') {
-      message = 'Select A Book!';
-    } else if (keyboard.length && start === 'start') {
-      message = 'Test Passed Successfully!';
-    } else {
-      message = 'Book Not Found please check your spelling!';
+
+    for (let { title, downloadUrl, imageUrl } of keyboard) {
+      await bot.sendPhoto(chatId, imageUrl, {
+        caption: `<a href="${downloadUrl}"><strong>${title}</strong></a>`,
+        parse_mode: 'HTML',
+      });
     }
 
-    await bot.sendMessage(chatId, message, {
-      reply_markup: {
-        inline_keyboard: keyboard,
-      },
-    });
-
-    if (keyboard.length && start !== 'start') {
-      await bot.sendMessage(
-        chatId,
-        `If you don't see what you're looking for try adding the author's name. E.g: Thinking Fast and Slow, Daniel Kahneman`
-      );
-    } else if (keyboard.length && start === 'start') {
-      await bot.sendMessage(chatId, `Try Sending: Think Fast and Slow`);
+    if (keyboard.length) {
+      if (!start) {
+        await bot.sendMessage(
+          chatId,
+          `If you don't see what you're looking for try adding the author's name. E.g: Thinking Fast and Slow, Daniel Kahneman`
+        );
+      } else {
+        await bot.sendMessage(chatId, `<strong>Test Passed Successfully!</strong>`, {
+          parse_mode: 'HTML',
+        });
+        await bot.sendMessage(chatId, `Try Sending: Think Fast and Slow`);
+      }
+    } else {
+      await bot.sendMessage(chatId, `Book Not Found please check your spelling!`);
     }
   } catch (error) {
     bot.sendMessage(chatId, `Sorry We're unable to process you're reqiest for now`);
@@ -68,34 +69,36 @@ const bookOptions = async (chatId, book, start) => {
   }
 };
 
-const fetchData = async (book) => {
+const fetchData = async (bookTitle) => {
   try {
     const options = {
       mirror: 'http://gen.lib.rus.ec',
-      query: book,
+      query: bookTitle,
       count: 5,
       // sort_by: 'extension',
       // reverse: true,
       fields: ['Title', { extension: 'pdf' }],
     };
 
-    const data = await libgen.search(options);
-    // if (data instanceof TypeError) {
-    //   throw new Error('Spelling Error')
-    // }
-    let n = data.length;
+    const listOfBooks = await libgen.search(options);
     let keyboard = [];
-    while (n--) {
-      const collect = [
-        {
-          text: `(${data[n].extension}) ${data[n].title}`,
-          url: `http://80.82.78.35/get.php?md5=${data[
-            n
-          ].md5.toLowerCase()}&key=S2NWTD621CJ0BAX9&mirr=1`,
-        },
-      ];
 
-      keyboard.push(collect);
+    for (let book of listOfBooks) {
+      let imageUrl = 'http://libgen.rs/covers/';
+
+      // if coverurl exits and it's not empty or white space
+      if (book.coverurl && !/^\s*$/.test(book.coverurl)) {
+        imageUrl += book.coverurl;
+      } else {
+        imageUrl = 'https://readersend.com/wp-content/uploads/2018/04/book-sample_preview-1.png';
+      }
+
+      const details = {
+        title: `[${book.extension.toUpperCase()}] ${book.title}`,
+        downloadUrl: `http://80.82.78.35/get.php?md5=${book.md5.toLowerCase()}&key=S2NWTD621CJ0BAX9&mirr=1`,
+        imageUrl,
+      };
+      keyboard.push(details);
     }
     return keyboard;
   } catch (error) {
